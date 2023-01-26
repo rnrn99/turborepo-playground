aab1364844e4f19c00244d0db4d5aa466b505dce
rnrn99 <giu0213@naver.com>
2023-01-26 22:11:55 +0900

    feat: add internal package test

--- apps/docs/pages/index.tsx
+++ apps/docs/pages/index.tsx
@@ -1,9 +1,11 @@
 import { Button } from "ui";
+import { testFunc } from "utils";
 
 export default function Docs() {
   return (
     <div>
       <h1>Docs</h1>
+      <button onClick={testFunc}>log</button>
       <Button />
     </div>
   );
